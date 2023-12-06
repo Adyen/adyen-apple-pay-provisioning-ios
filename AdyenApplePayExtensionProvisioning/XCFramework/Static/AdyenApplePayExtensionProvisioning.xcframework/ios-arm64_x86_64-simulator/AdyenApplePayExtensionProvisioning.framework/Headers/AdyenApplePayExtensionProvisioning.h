cf6535c2af2563f45fb7443332808389e4258300
bavarskis <bavarskiss@gmail.com>
2023-12-06 02:50:21 -0800

    1.0.0

---
+++ AdyenApplePayExtensionProvisioning/XCFramework/Static/AdyenApplePayExtensionProvisioning.xcframework/ios-arm64_x86_64-simulator/AdyenApplePayExtensionProvisioning.framework/Headers/AdyenApplePayExtensionProvisioning.h
@@ -0,0 +1,18 @@
+//
+//  AdyenApplePayExtensionProvisioning.h
+//  AdyenApplePayExtensionProvisioning
+//
+//  Copyright (c) 2023 Adyen N.V.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdyenApplePayExtensionProvisioning.
+FOUNDATION_EXPORT double AdyenApplePayExtensionProvisioningVersionNumber;
+
+//! Project version string for AdyenApplePayExtensionProvisioning.
+FOUNDATION_EXPORT const unsigned char AdyenApplePayExtensionProvisioningVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdyenApplePayExtensionProvisioning/PublicHeader.h>
+
+
